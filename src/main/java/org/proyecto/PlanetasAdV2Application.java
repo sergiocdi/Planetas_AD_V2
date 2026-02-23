196833429d38bfcb0af39b4b572faec69c54ab98
sergiocdi <81166487+sergiocdi@users.noreply.github.com>
2026-02-23 18:19:11 +0100

    Configure project for WAR deployment to Azure

--- src/main/java/org/proyecto/PlanetasAdV2Application.java
+++ src/main/java/org/proyecto/PlanetasAdV2Application.java
@@ -2,9 +2,16 @@ package org.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PlanetasAdV2Application {
+public class PlanetasAdV2Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PlanetasAdV2Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlanetasAdV2Application.class, args);
